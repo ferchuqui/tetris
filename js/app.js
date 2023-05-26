39188a621b52de2b2f3c71ddf831a41d552d31d3
Luis Fernando Farji <fernando.farji@gmail.com>
2023-05-25 21:13:44 -0300

    taken

--- js/app.js
+++ js/app.js
@@ -1,3 +1,74 @@
 document.addEventListener('DOMContentLoaded',() =>{
-  
+  const grid = document.querySelector('.grid')
+  let squares = Array.from(document.querySelectorAll('.grid div'))
+  const ScoreDisplay = document.querySelector('#score')
+  const StartBtn = document.querySelector('#start-button')
+  const width = 10
+
+  //The Tetrominoes
+  const lTetromino = [
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
+  const zTetromino = [
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
+
+  const tTetromino = [
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
+
+  const oTetromino = [
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
+
+  const iTetromino = [
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+  const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  let currentPosition = 4
+  let currentRotation = 0
+
+  //Selecciona un tetromino al azar y su primera rotacion
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  console.log(random)
+  let current = theTetrominoes[random][currentRotation]
+
+  //draw the Tetromino
+  function draw() {
+    current.forEach(index => {
+      squares[currentPosition + index].classList.add('tetromino')
+    })
+  }
+  function undraw(){
+    current.forEach(index => {
+      squares[currentPosition + index].classList.remove('tetromino')
+    })
+  }
+  //el tetromino se movera cada segundo
+
+  timerId = setInterval(moveDown, 1000)
+
+
+  //move down function
+  function moveDown() {
+    undraw ()
+    currentPosition += width
+    draw()
+  }
 })
